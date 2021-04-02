1c67fb709d209d51e5339bf52195520c3f8c84c9
Bence Papp <bencepapp1101@gmail.com>
2021-04-02 18:09:18 +0200

    OnePlayer.cs created

---
+++ FiveFeetBelowGame/FiveFeetBelowGame/VM/OnePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveFeetBelowGame.VM
+{
+      class OnePlayer
+      {
+            public double DX { get; set; }
+
+            public double DY { get; set; }
+
+            public OnePlayer()
+            {
+
+            }
+      }
+}
