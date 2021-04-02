b5abe33a7a0ee30a3167e4b3a4d4c79ae5b16fed
Bence Papp <bencepapp1101@gmail.com>
2021-04-02 17:48:44 +0200

    GameModel.cs created

---
+++ FiveFeetBelowGame/FiveFeetBelowGame/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1814:Prefer jagged arrays over multidimensional", Justification = "Unnecessary", Scope = "member", Target = "~P:FiveFeetBelowGame.VM.GameModel.Rock")]
+[assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "Unnecessary", Scope = "member", Target = "~P:FiveFeetBelowGame.VM.GameModel.Rock")]
