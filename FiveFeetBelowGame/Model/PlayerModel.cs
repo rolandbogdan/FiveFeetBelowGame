730b1c84aa8e719aab22bb0bd174560ce64e4323
Bence Papp <bencepapp1101@gmail.com>
2021-04-06 15:35:19 +0200

    new files added

---
+++ FiveFeetBelowGame/Model/PlayerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+      public class PlayerModel : IGameModel
+      {
+            public string ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public int DX { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public int DY { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public int HealthPoints { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+      }
+}
---
+++ FiveFeetBelowGame/Repository/PlayerRepo.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="PlayerRepo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Repository
+{
+      using Model;
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+
+      public class PlayerRepo : IStorageRepository<PlayerModel>
+      {
+            public void Delete(PlayerModel toDelete)
+            {
+                  throw new NotImplementedException();
+            }
+
+            public IQueryable<PlayerModel> GetAll()
+            {
+                  throw new NotImplementedException();
+            }
+
+            public PlayerModel GetOne(string id)
+            {
+                  throw new NotImplementedException();
+            }
+
+            public void Insert(PlayerModel toInsert)
+            {
+                  throw new NotImplementedException();
+            }
+
+            public void SaveChanges()
+            {
+                  throw new NotImplementedException();
+            }
+      }
+}
