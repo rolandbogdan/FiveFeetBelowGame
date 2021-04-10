a8540b1d622677120c6e25e614504905d5cf44f2
Bence Papp <bencepapp1101@gmail.com>
2021-04-10 19:51:51 +0200

    tests for the highscore is done

---
+++ FiveFeetBelowGame/Logic/HighscoreLogic.cs
@@ -0,0 +1,76 @@
+﻿// <copyright file="HighscoreLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Logic
+{
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+      using Model;
+      using Repository;
+
+      /// <summary>
+      /// This class is represent the logc of the highscore.
+      /// </summary>
+      public class HighscoreLogic
+      {
+            private IStorageRepository<Highscore> highscoreRepo;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="HighscoreLogic"/> class.
+            /// </summary>
+            /// <param name="highscoreRepo">IStorageRepository Highscore type parameter.</param>
+            public HighscoreLogic(IStorageRepository<Highscore> highscoreRepo)
+            {
+                  this.highscoreRepo = highscoreRepo;
+            }
+
+            /// <summary>
+            /// Save method to save a highscore.
+            /// </summary>
+            public void Save()
+            {
+                  this.highscoreRepo.Save();
+            }
+
+            /// <summary>
+            /// Insert method to add a new highscore.
+            /// </summary>
+            /// <param name="hs">Highscore type parameter what we want to save.</param>
+            public void Insert(Highscore hs)
+            {
+                  this.highscoreRepo.Insert(hs);
+            }
+
+            /// <summary>
+            /// Delete method to delete a highscore.
+            /// </summary>
+            /// <param name="hs">Highscore type parameter what we want to delete.</param>
+            public void Delete(Highscore hs)
+            {
+                  this.highscoreRepo.Delete(hs);
+            }
+
+            /// <summary>
+            /// GetOneHighscore method to find one highscore by id.
+            /// </summary>
+            /// <param name="id">String type parameter what we want to find.</param>
+            /// <returns>Return a highscore.</returns>
+            public Highscore GetOneHighscore(string id)
+            {
+                  return this.highscoreRepo.GetOne(id);
+            }
+
+            /// <summary>
+            /// GetAllHighscore to list all of the highscores.
+            /// </summary>
+            /// <returns>Returns an IQueryable Highscore type.</returns>
+            public IQueryable<Highscore> GetAllHighscore()
+            {
+                  return this.highscoreRepo.GetAll();
+            }
+      }
+}
--- FiveFeetBelowGame/Model/Highscore.cs
+++ FiveFeetBelowGame/Model/Highscore.cs
@@ -4,49 +4,56 @@
 
 namespace Model
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
 
-    /// <summary>
-    /// The highscores of the game.
-    /// </summary>
-    public class Highscore
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Highscore"/> class.
-        /// </summary>
-        /// <param name="playername"> The name of the player. </param>
-        /// <param name="deepestpoint"> The deepest point the player has reached. </param>
-        /// <param name="pickaxelvl"> The level of the player's pickaxe. </param>
-        public Highscore(string playername, int deepestpoint, int pickaxelvl)
-        {
-            this.HsID = Guid.NewGuid().ToString();
-            this.PlayerName = playername;
-            this.DeepestPoint = deepestpoint;
-            this.PickaxeLvl = pickaxelvl;
-        }
+      /// <summary>
+      /// The highscores of the game.
+      /// </summary>
+      public class Highscore
+      {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Highscore"/> class.
+            /// </summary>
+            /// <param name="playername"> The name of the player. </param>
+            /// <param name="deepestpoint"> The deepest point the player has reached. </param>
+            /// <param name="pickaxelvl"> The level of the player's pickaxe. </param>
+            public Highscore(string playername, int deepestpoint, int pickaxelvl)
+            {
+                  this.HsID = Guid.NewGuid().ToString();
+                  this.PlayerName = playername;
+                  this.DeepestPoint = deepestpoint;
+                  this.PickaxeLvl = pickaxelvl;
+            }
 
-        /// <summary>
-        /// Gets or sets the id of the highscore save.
-        /// </summary>
-        public string HsID { get; set; }
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Highscore"/> class.
+            /// </summary>
+            public Highscore()
+            {
+            }
 
-        /// <summary>
-        /// Gets or sets the name of the player.
-        /// </summary>
-        public string PlayerName { get; set; }
+            /// <summary>
+            /// Gets or sets the id of the highscore save.
+            /// </summary>
+            public string HsID { get; set; }
 
-        /// <summary>
-        /// Gets or sets the deepest point the player has reached.
-        /// </summary>
-        public int DeepestPoint { get; set; }
+            /// <summary>
+            /// Gets or sets the name of the player.
+            /// </summary>
+            public string PlayerName { get; set; }
 
-        /// <summary>
-        /// Gets or sets the level of the player's pickaxe.
-        /// </summary>
-        public int PickaxeLvl { get; set; }
-    }
+            /// <summary>
+            /// Gets or sets the deepest point the player has reached.
+            /// </summary>
+            public int DeepestPoint { get; set; }
+
+            /// <summary>
+            /// Gets or sets the level of the player's pickaxe.
+            /// </summary>
+            public int PickaxeLvl { get; set; }
+      }
 }
