852c6b16409f67b32b55a5701f8f1a7b2f74ac56
Roland Bogdan <bogdanroland07@gmail.com>
2021-04-10 17:33:29 +0200

    data created, hs repo started

---
+++ FiveFeetBelowGame/Data/HighscoreContext.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="HighscoreContext.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+[assembly: System.CLSCompliant(false)]
+
+namespace Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Model;
+
+    /// <summary>
+    /// The context class for the highscores.
+    /// </summary>
+    public class HighscoreContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighscoreContext"/> class.
+        /// </summary>
+        /// <param name="opt">The db context options. </param>
+        public HighscoreContext(DbContextOptions<HighscoreContext> opt)
+            : base(opt)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighscoreContext"/> class.
+        /// </summary>
+        public HighscoreContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Gets or sets the dbset of highscores.
+        /// </summary>
+        public DbSet<Highscore> Highscores { get; set; }
+    }
+}
---
+++ FiveFeetBelowGame/Repository/HighscoreRepo.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="HighscoreRepo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Model;
+
+    /// <summary>
+    /// The repository class for the highscores.
+    /// </summary>
+    public class HighscoreRepo : IStorageRepository<Highscore>
+    {
+        // private
+        public void Delete(Highscore toDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Highscore> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Highscore GetOne(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Highscore toInsert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
