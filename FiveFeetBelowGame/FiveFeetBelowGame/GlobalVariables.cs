f99d5153e9c43b4f1dadc8b5ff1128bbf60389e6
Bence Papp <bencepapp1101@gmail.com>
2021-05-08 16:14:47 +0200

    MainWindow modified

--- FiveFeetBelowGame/FiveFeetBelowGame/GlobalVariables.cs
+++ FiveFeetBelowGame/FiveFeetBelowGame/GlobalVariables.cs
@@ -4,30 +4,35 @@
 
 namespace FiveFeetBelowGame
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
 
-    /// <summary>
-    /// Contains global variables for the game.
-    /// </summary>
-    public static class GlobalVariables
-    {
-        /// <summary>
-        /// Gets or sets the game file path.
-        /// </summary>
-        public static string GamefilePath { get; set; }
+      /// <summary>
+      /// Contains global variables for the game.
+      /// </summary>
+      public static class GlobalVariables
+      {
+            /// <summary>
+            /// Gets or sets the game file path.
+            /// </summary>
+            public static string GamefilePath { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether we are loading a game or not.
-        /// </summary>
-        public static bool GameLoad { get; set; }
+            /// <summary>
+            /// Gets or sets a value indicating whether we are loading a game or not.
+            /// </summary>
+            public static bool GameLoad { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether we can autosave or not.
-        /// </summary>
-        public static bool CanAutosave { get; set; }
-    }
+            /// <summary>
+            /// Gets or sets a value indicating whether we can autosave or not.
+            /// </summary>
+            public static bool CanAutosave { get; set; }
+
+            /// <summary>
+            /// Gets or sets the name of the save.
+            /// </summary>
+            public static string SaveName { get; set; }
+      }
 }
--- FiveFeetBelowGame/FiveFeetBelowGame/UI/GameControl.cs
+++ FiveFeetBelowGame/FiveFeetBelowGame/UI/GameControl.cs
@@ -4,194 +4,200 @@
 
 namespace FiveFeetBelowGame.UI
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Threading;
-    using FiveFeetBelowGame.BL;
-    using FiveFeetBelowGame.VM;
+      using System;
+      using System.Collections.Generic;
+      using System.Linq;
+      using System.Text;
+      using System.Threading.Tasks;
+      using System.Windows;
+      using System.Windows.Input;
+      using System.Windows.Media;
+      using System.Windows.Threading;
+      using FiveFeetBelowGame.BL;
+      using FiveFeetBelowGame.VM;
 
-    /// <summary>
-    /// GameControl class for the game.
-    /// </summary>
-    public class GameControl : FrameworkElement
-    {
-        /// <summary>
-        /// An instance of the descendant class.
-        /// </summary>
-        private GameLogic logic;
+      /// <summary>
+      /// GameControl class for the game.
+      /// </summary>
+      public class GameControl : FrameworkElement
+      {
+            /// <summary>
+            /// An instance of the descendant class.
+            /// </summary>
+            private GameLogic logic;
 
-        /// <summary>
-        /// An instance of the descendant class.
-        /// </summary>
-        private Renderer renderer;
+            /// <summary>
+            /// An instance of the descendant class.
+            /// </summary>
+            private Renderer renderer;
 
-        /// <summary>
-        /// An instance of the descendant class.
-        /// </summary>
-        private GameModel model;
+            /// <summary>
+            /// An instance of the descendant class.
+            /// </summary>
+            private GameModel model;
 
-        /// <summary>
-        /// An instance of the descendant class.
-        /// </summary>
-        private DispatcherTimer tickTimer;
+            /// <summary>
+            /// An instance of the descendant class.
+            /// </summary>
+            private DispatcherTimer tickTimer;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GameControl"/> class.
-        /// </summary>
-        public GameControl()
-        {
-            this.Loaded += this.GameControl_Loaded;
-        }
-
-        /// <inheritdoc/>
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            if (this.renderer != null)
+            /// <summary>
+            /// Initializes a new instance of the <see cref="GameControl"/> class.
+            /// </summary>
+            public GameControl()
             {
-                drawingContext?.DrawDrawing(this.renderer.BuildDrawing());
-            }
-        }
-
-        private void GameControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.model = new GameModel(this.ActualWidth, this.ActualHeight);
-
-            if (string.IsNullOrEmpty(GlobalVariables.GamefilePath))
-            {
-                GlobalVariables.GameLoad = false;
-                this.logic = new GameLogic(this.model, "testfile.json");
-            }
-            else
-            {
-                GlobalVariables.GameLoad = true;
-                this.logic = new GameLogic(this.model, GlobalVariables.GamefilePath);
-                GlobalVariables.GamefilePath = string.Empty;
+                  this.Loaded += this.GameControl_Loaded;
             }
 
-            this.model = this.logic.Model;
-            this.model.GameHeight = this.ActualHeight;
-            this.model.GameWidth = this.ActualWidth;
-            this.renderer = new Renderer(this.model);
-            Window win = Window.GetWindow(this);
-            if (win != null)
+            /// <inheritdoc/>
+            protected override void OnRender(DrawingContext drawingContext)
             {
-                this.tickTimer = new DispatcherTimer();
-                this.tickTimer.Interval = TimeSpan.FromMilliseconds(40); // fps?
-                this.tickTimer.Tick += this.TickTimer_Tick;
-                this.tickTimer.Start();
-
-                win.KeyDown += this.Win_KeyDown;
-                this.MouseDown += this.GameControl_MouseDown;
+                  if (this.renderer != null)
+                  {
+                        drawingContext?.DrawDrawing(this.renderer.BuildDrawing());
+                  }
             }
 
-            this.InvalidateVisual();
-        }
-
-        private void GameControl_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Point mousePos = e.GetPosition(this);
-            Point tilePos = this.logic.GetTilePos(mousePos);
-
-            int tpx = (int)tilePos.X;
-            int tpy = (int)tilePos.Y;
-
-            if (tpx < this.model.Blocks.GetLength(0) &&
-                tpy < this.model.Blocks.GetLength(1) &&
-                this.model.Blocks[tpx, tpy] != null &&
-                this.model.Player != null &&
-                this.logic.IsNeighboring(tpx, tpy))
+            private void GameControl_Loaded(object sender, RoutedEventArgs e)
             {
-                this.model.Blocks[tpx, tpy].DamageTaken(
-                this.model.Player.InflictDamage(),
-                this.model.Player);
-                if (this.model.Blocks[tpx, tpy].HealthPoints <= 0)
-                {
-                    if (this.model.Blocks[tpx, tpy] as OneBlock != null)
-                    {
-                        this.logic.PlayerGainedMoney((this.model.Blocks[tpx, tpy] as OneBlock).Value);
-                    }
-                    else if (this.model.Blocks[tpx, tpy] as OneMonster != null)
-                    {
-                        this.logic.PlayerGainedMoney((this.model.Blocks[tpx, tpy] as OneMonster).Value);
-                    }
+                  this.model = new GameModel(this.ActualWidth, this.ActualHeight);
 
-                    this.model.Blocks[tpx, tpy] = new OneBlock(tpx, tpy, BlockType.Air);
-                }
-            }
-        }
+                  if (string.IsNullOrEmpty(GlobalVariables.GamefilePath))
+                  {
+                        GlobalVariables.GameLoad = false;
+                        this.logic = new GameLogic(this.model, "testfile.json");
+                  }
+                  else
+                  {
+                        GlobalVariables.GameLoad = true;
+                        this.logic = new GameLogic(this.model, GlobalVariables.GamefilePath);
+                        GlobalVariables.GamefilePath = string.Empty;
+                  }
 
-        private void TickTimer_Tick(object sender, EventArgs e)
-        {
-            this.model.Blocks = this.logic.GetRenderedBlocks();
-            this.logic.Gravity();
-            this.logic.CheckIfHighscore();
-            if (GlobalVariables.CanAutosave)
-            {
-                this.logic.AutoSave();
-                GlobalVariables.CanAutosave = false;
+                  this.model = this.logic.Model;
+                  this.model.GameHeight = this.ActualHeight;
+                  this.model.GameWidth = this.ActualWidth;
+                  this.renderer = new Renderer(this.model);
+                  Window win = Window.GetWindow(this);
+                  if (win != null)
+                  {
+                        this.tickTimer = new DispatcherTimer();
+                        this.tickTimer.Interval = TimeSpan.FromMilliseconds(40); // fps?
+                        this.tickTimer.Tick += this.TickTimer_Tick;
+                        this.tickTimer.Start();
+
+                        win.KeyDown += this.Win_KeyDown;
+                        this.MouseDown += this.GameControl_MouseDown;
+                  }
+
+                  this.InvalidateVisual();
             }
 
-            this.InvalidateVisual();
-        }
-
-        private void Win_KeyDown(object sender, KeyEventArgs e)
-        {
-            bool isPaused = false;
-
-            switch (e.Key)
+            private void GameControl_MouseDown(object sender, MouseButtonEventArgs e)
             {
-                case System.Windows.Input.Key.W:
-                    if (!isPaused)
-                    {
-                        this.logic.Move(0, -1);
-                    }
+                  Point mousePos = e.GetPosition(this);
+                  Point tilePos = this.logic.GetTilePos(mousePos);
 
-                    break;
-                case System.Windows.Input.Key.S:
-                    if (!isPaused)
-                    {
-                        this.logic.Move(0, 1);
-                    }
+                  int tpx = (int)tilePos.X;
+                  int tpy = (int)tilePos.Y;
 
-                    break;
-                case System.Windows.Input.Key.A:
-                    if (!isPaused)
-                    {
-                        this.logic.Move(-1, 0);
-                    }
+                  if (tpx < this.model.Blocks.GetLength(0) &&
+                      tpy < this.model.Blocks.GetLength(1) &&
+                      this.model.Blocks[tpx, tpy] != null &&
+                      this.model.Player != null &&
+                      this.logic.IsNeighboring(tpx, tpy))
+                  {
+                        this.model.Blocks[tpx, tpy].DamageTaken(
+                        this.model.Player.InflictDamage(),
+                        this.model.Player);
+                        if (this.model.Blocks[tpx, tpy].HealthPoints <= 0)
+                        {
+                              if (this.model.Blocks[tpx, tpy] as OneBlock != null)
+                              {
+                                    this.logic.PlayerGainedMoney((this.model.Blocks[tpx, tpy] as OneBlock).Value);
+                              }
+                              else if (this.model.Blocks[tpx, tpy] as OneMonster != null)
+                              {
+                                    this.logic.PlayerGainedMoney((this.model.Blocks[tpx, tpy] as OneMonster).Value);
+                              }
 
-                    break;
-                case System.Windows.Input.Key.D:
-                    if (!isPaused)
-                    {
-                        this.logic.Move(1, 0);
-                    }
-
-                    break;
-                case Key.Escape:
-                    if (MessageBox.Show("Are you sure?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
-                    {
-                        MainWindow mw = new MainWindow();
-                        Window win = Window.GetWindow(this);
-
-                        this.logic.SaveGame($"{DateTime.Now.ToString()}.json");
-
-                        win.Close();
-                        mw.Show();
-                    }
-
-                    break;
-                default:
-                    break;
+                              this.model.Blocks[tpx, tpy] = new OneBlock(tpx, tpy, BlockType.Air);
+                        }
+                  }
             }
 
-            this.InvalidateVisual();
-        }
-    }
+            private void TickTimer_Tick(object sender, EventArgs e)
+            {
+                  this.model.Blocks = this.logic.GetRenderedBlocks();
+                  this.logic.Gravity();
+                  this.logic.CheckIfHighscore();
+                  if (GlobalVariables.CanAutosave)
+                  {
+                        this.logic.AutoSave();
+                        GlobalVariables.CanAutosave = false;
+                  }
+
+                  this.InvalidateVisual();
+            }
+
+            private void Win_KeyDown(object sender, KeyEventArgs e)
+            {
+                  bool isPaused = false;
+
+                  switch (e.Key)
+                  {
+                        case System.Windows.Input.Key.W:
+                              if (!isPaused)
+                              {
+                                    this.logic.Move(0, -1);
+                              }
+
+                              break;
+                        case System.Windows.Input.Key.S:
+                              if (!isPaused)
+                              {
+                                    this.logic.Move(0, 1);
+                              }
+
+                              break;
+                        case System.Windows.Input.Key.A:
+                              if (!isPaused)
+                              {
+                                    this.logic.Move(-1, 0);
+                              }
+
+                              break;
+                        case System.Windows.Input.Key.D:
+                              if (!isPaused)
+                              {
+                                    this.logic.Move(1, 0);
+                              }
+
+                              break;
+                        case Key.Escape:
+                              if (MessageBox.Show("Are you sure?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                              {
+                                    MainWindow mw = new MainWindow();
+                                    Window win = Window.GetWindow(this);
+                                    if (string.IsNullOrEmpty(GlobalVariables.SaveName))
+                                    {
+                                          this.logic.SaveGame($"{GlobalVariables.SaveName}.json");
+                                    }
+                                    else
+                                    {
+                                          this.logic.AutoSave();
+                                    }
+
+                                    win.Close();
+                                    mw.Show();
+                              }
+
+                              break;
+                        default:
+                              break;
+                  }
+
+                  this.InvalidateVisual();
+            }
+      }
 }
